caa71137f2597e4066f3e7671fc677bc44cb0497
dahailang <wangjn1130@163.com>
2017-09-19 22:29:21 +0800

    启动脚本自动创建目录

--- src/main/resources/bin/restart.sh
+++ src/main/resources/bin/restart.sh
@@ -1,5 +1,6 @@
 export JAVA_HOME=/usr/java/jdk1.8.0_144
 export PATH=$JAVA_HOME/bin:$PATH
+mkdir -p /home/tengine/noteblog/logs
 ##stop
 /bin/sh /home/tengine/noteblog/bin/stopnoteblog.sh 
 ##start noteblog
