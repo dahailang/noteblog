99db936cf71038c0b0778964bcc6911a309292e7
dahailang <wangjn1130@163.com>
2017-10-28 10:35:45 +0800

    初始化展示数据

--- src/main/resources/static/js/note/content.js
+++ src/main/resources/static/js/note/content.js
@@ -1,17 +1,27 @@
-$(function() {                
+$(function() {
+	//1.初始化数据获取
+	//2.左侧菜单树展开
+
+	//3.默认目录区域选项
+	//4.内容区域初始化
 	var noteEditor;
-    $("#noteblog").click(function(){                    
-		$.getScript("../../editormd/editormd.js", function() {
-	        $("#layout").html("<div id='editormd'></div>");
-	        noteEditor = editormd("editormd", {
-	            markdown : "### 动态创建 Editor.md\r\n\r\nDynamic create Editor.md",
-	            //readOnly:true,
-	            watch:false,
-	            path : '../../editormd/lib/',
-	            onload: function() {
-	            	this.previewing();
-	            }
-	        });
+	$.getScript("../../editormd/editormd.js", function() {
+		$("#layout").html("<div id='editormd'></div>");
+		noteEditor = editormd("editormd", {
+			markdown : "### 动态创建 Editor.md\r\n\r\nDynamic create Editor.md",
+			//readOnly:true,
+			watch:false,
+			path : '../../editormd/lib/',
+			onload: function() {
+				this.previewing();
+			}
 		});
-    });  
+	});
+    
+	function expandleft(treeid){
+	    var zTree_Menu = $.fn.zTree.getZTreeObj("treeDemo");  
+	    var node = zTree_Menu.getNodeByParam("id",pid);  
+	    zTree_Menu.selectNode(node,true);//指定选中ID的节点  
+	    zTree_Menu.expandNode(node, true, false);//指定选中ID节点展开  
+	}
 });  
