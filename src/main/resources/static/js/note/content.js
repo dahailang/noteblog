66716e23f713a348b77d5b125d19cf13609887fb
dahailang <wangjn1130@163.com>
2017-11-09 21:28:51 +0800

    编辑页面修改

--- src/main/resources/static/js/note/content.js
+++ src/main/resources/static/js/note/content.js
@@ -18,7 +18,7 @@ $(function() {
 	//4.内容区域初始化
 	var noteEditor;
 	$.getScript("../../editormd/editormd.js", function() {
-		$("#layout").html("<div id='editormd'></div>");
+		$("#editormd-view").html("<div id='editormd'></div>");
 		noteEditor = editormd("editormd", {
 			markdown : "### 动态创建 Editor.md\r\n\r\nDynamic create Editor.md",
 			//readOnly:true,
